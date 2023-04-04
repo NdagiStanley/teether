e193578ba4ead1ae91855deae6203c44e4e400e3
Stanley Ndagi <ndagis@gmail.com>
2023-04-04 11:42:17 +0300

    Anchor tags - split test

--- src/.vuepress/components/Mantra.vue
+++ src/.vuepress/components/Mantra.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home-div">
-    <div>Mantra: <i>invoke the builder within and serve</i> <a href="/me#what-do-i-do" target="_blank" rel="noopener noreferrer">More ...</a></div>
+    <div>Mantra: <a href="/me/#what-do-i-do" rel="noopener noreferrer"><i>invoke the builder within and serve</i></a></div>
     <br/>
-    <div>Visionary at TechKln <a href="https://techkln.org" target="_blank" rel="noopener noreferrer">More ...</a></div>
-    <div>Global citizen as *salt* and *light* <a href="/kingdombiz" target="_blank" rel="noopener noreferrer">More ...</a></div>
+    <div>Visionary at <a href="https://techkln.org" target="_blank" rel="noopener noreferrer">TechKln</a></div>
+    <div>Global citizen as <a href="/kingdombiz" target="_blank" rel="noopener noreferrer"><b>salt</b> and <b>light</b></a></div>
     <!-- <div>Mentor</div> -->
   </div>
 </template>
